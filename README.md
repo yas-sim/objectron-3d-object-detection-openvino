4cbba965b460522cd1627b69682c6ed537a2c45f
Yasunori Shimura <63567854+yas-sim@users.noreply.github.com>
2020-06-28 21:12:20 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,63 @@
+
+# Overview
+This is a sample implementaion of 3D object detection application using Intel(r) Distribution of OpenVINO(tm) Toolkit. The program uses `objectron` model from [Google *mediapipe* project](https://github.com/google/mediapipe) to detect the objects from the input images.  
+In this project, I took the `objectron` model in TF SavedModel format from [PINTO model zoo](https://github.com/PINTO0309/PINTO_model_zoo) in the GitHub.  
+
+これはIntel(r) Distribution of OpenVINO(tm) Toolkitを使用した3D物体検出アプリケーションの参考実装です。プログラムは[Google *mediapipe*プロジェクト](https://github.com/google/mediapipe)の`objectron`モデルを使用しています。  
+このプロジェクトではPINTOさんの[PINTO model zoo](https://github.com/PINTO0309/PINTO_model_zoo)からTF SavedModelフォーマットのモデルデータを使用させて頂いています。  
+
+**Example of Detection result**  
+![output](resources/output.jpg)  
+![heatmap](resources/heatmap.png)
+
+## 1. Prerequisites
+* [Intel Distribution of OpenVINO toolkit 2020.2 (or newer)](https://software.intel.com/content/www/us/en/develop/tools/openvino-toolkit.html)
+
+## 2. Download Objectron TensorFlow model from PINTO model zoo
+### 2.1 Download the model download script
+ Download `download.sh` from [https://github.com/PINTO0309/PINTO_model_zoo/tree/master/36_Objectron/01_float32](https://github.com/PINTO0309/PINTO_model_zoo/tree/master/36_Objectron/01_float32)
+### 2.2 Run `download.sh` to get `object_detection_3d_chair.pb`  
+
+```sh
+ ./download.sh
+```
+
+## 3. Convert objectron TF model into OpenVINO IR model
+
+Use `Model Optimizer (MO)` to convert the TF model into IR model.
+
+```sh
+python3 ${INTEL_OPENVINO_DIR}/deployment_tools/model_optimizer/mo.py \
+ --saved_model_dir saved_model_object_detection_3d_chair \
+ --scale 255 \
+ --output_dir objectron_chair \
+ --data_type FP16
+```
+An IR model `./objectron_chair/saved_model.xml|.bin` will be generated.
+
+## 4. Run sample program
+
+```sh
+python3 objectron.py [-m model.xml] [-i input_image] [--heatmap]
+```
+*Options*  
+`-m`: IR model path (default: `./objectron_chair/saved_model.xml`)  
+`-i`: Input image path. A webCam will be used as the input when you specify `cam` (default: `chair.jpg`)  
+`--heatmap`: Display heatmap
+
+
+
+**Command line example:**
+```sh
+ $ python3 objectron.py -m ./objectron_chair/object_detection_3d_chair.xml -i cam --heatmap
+```
+
+## 5. Test Environment
+- Ubuntu 18.04 / Windows 10 1909  
+- OpenVINO 2020.3 LTS  
+
+## See Also  
+* [Google AI Blog, Real-Time 3D Object Detection on Mobile Devices with MediaPipe](https://ai.googleblog.com/2020/03/real-time-3d-object-detection-on-mobile.html)
+* [GitHub, Google *mediapipe* project](https://github.com/google/mediapipe)  
+* [GitHub, PINTO model zoo](https://github.com/PINTO0309)  
+* [Intel(r) Distribution of OpenVINO(tm) Toolkit](https://software.intel.com/content/www/us/en/develop/tools/openvino-toolkit.html)
---
+++ objectron.py
@@ -0,0 +1,132 @@
+import os
+import sys
+import argparse
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from scipy.ndimage.filters import maximum_filter
+
+from openvino.inference_engine import IECore
+
+def detect_peak(image, filter_size=3, order=0.5):
+    local_max = maximum_filter(image, footprint=np.ones((filter_size, filter_size)), mode='constant')
+    detected_peaks = np.ma.array(image,mask=~(image == local_max))
+    
+    temp = np.ma.array(detected_peaks, mask=~(detected_peaks >= detected_peaks.max() * order))
+    peaks_index = np.where((temp.mask != True))
+    return peaks_index
+
+def decode(hm, displacements, threshold=0.8):
+    hm = hm.reshape(hm.shape[2:])     # (40,30)
+    peaks = detect_peak(hm)
+    peakX = peaks[1]
+    peakY = peaks[0]
+
+    scaleX = hm.shape[1]
+    scaleY = hm.shape[0]
+    objs = []
+    for x,y in zip(peakX, peakY):
+        conf = hm[y,x]
+        if conf<threshold:
+            continue
+        points=[]
+        for i in range(8):
+            dx = displacements[0, i*2  , y, x]
+            dy = displacements[0, i*2+1, y, x]
+            points.append((x/scaleX+dx, y/scaleY+dy))
+        objs.append(points)
+    return objs
+
+def show_heatmap(hm):
+    h = hm.reshape((hm.shape[2],hm.shape[3],1))
+    h = cv2.resize(h, None, fx=10, fy=10)
+    cv2.imshow('heatmap', h)
+
+def draw_box(image, pts):
+    scaleX = image.shape[1]
+    scaleY = image.shape[0]
+
+    lines = [(0,1), (1,3), (0,2), (3,2), (1,5), (0,4), (2,6), (3,7), (5,7), (6,7), (6,4), (4,5)]
+    for line in lines:
+        pt0 = pts[line[0]]
+        pt1 = pts[line[1]]
+        pt0 = (int(pt0[0]*scaleX), int(pt0[1]*scaleY))
+        pt1 = (int(pt1[0]*scaleX), int(pt1[1]*scaleY))
+        cv2.line(image, pt0, pt1, (255,0,0))
+    
+    for i in range(8):
+        pt = pts[i]
+        pt = (int(pt[0]*scaleX), int(pt[1]*scaleY))
+        cv2.circle(image, pt, 8, (0,255,0), -1)
+        cv2.putText(image, str(i), pt,  cv2.FONT_HERSHEY_PLAIN, 2, (0,0,0), 2)
+
+def main(args):
+
+    base,ext = os.path.splitext(args.model)
+    if ext != '.xml':
+        print('Not .xml file is specified ', args.model)
+        sys.exit(-1)
+
+    ie = IECore()
+    net = ie.read_network(base+'.xml', base+'.bin')
+    exenet = ie.load_network(net, 'CPU')
+
+    inblobs =  (list(net.inputs.keys()))
+    outblobs = (list(net.outputs.keys()))
+    print(inblobs, outblobs)
+
+    inshapes  = [ net.inputs [i].shape for i in inblobs  ]
+    outshapes = [ net.outputs[i].shape for i in outblobs ]
+    print(inshapes, outshapes)
+
+    if args.input == 'cam':
+        cap = cv2.VideoCapture(0)
+
+    while True:
+        if args.input == 'cam':
+            _, img_orig = cap.read()
+        else:
+            img_file = args.input
+            img_orig = cv2.imread(img_file)
+
+        img = cv2.cvtColor(img_orig, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (inshapes[0][3], inshapes[0][2]))
+        img = img.transpose((2,0,1))
+
+        res = exenet.infer({inblobs[0]:img})
+
+        hm = res[outblobs[1]]
+        displacements = res[outblobs[0]]
+
+        # show heatmap
+        if args.heatmap == True:
+            show_heatmap(hm)
+
+        # decode inference result
+        objs = decode(hm, displacements, threshold=0.7)
+
+        # draw bbox
+        for obj in objs:
+            draw_box(img_orig, obj)
+        
+        if args.input == 'cam':
+            cv2.imshow('output', img_orig)
+            if cv2.waitKey(1)==27:
+                return
+        else:
+            cv2.imwrite('output.jpg', img_orig)
+            cv2.imshow('output', img_orig)
+            cv2.waitKey(0)
+            return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, default='chair.jpg', help='input image file name')
+    parser.add_argument('-m', '--model', type=str, default='./objectron_chair/saved_model.xml', help='FBFace IR model file name (*.xml)')
+    parser.add_argument('--heatmap', action='store_true', required=False, default=False, help='Display heatmap')
+    args = parser.parse_args()
+
+    main(args)
